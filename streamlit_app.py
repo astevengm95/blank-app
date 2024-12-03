d25e0ae19cff51fc1bae928c13d1166ffedf1b92
astevengm95 <steven.godoy95@ucuenca.edu.ec>
2024-12-03 12:20:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My nueva app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start bu"
 )
